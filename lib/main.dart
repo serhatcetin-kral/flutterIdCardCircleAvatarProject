99fa2e1204abaad10d64e474602300834792daac
serhatcetin-kral <serhat06.sc@gmail.com>
2023-06-14 11:19:53 +0200

    flutter circle Avatar id caerd

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:IdCard(),
+  ));
+}
+
+class IdCard extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("IdCard"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30,40,30,0),
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child:
+            CircleAvatar(backgroundImage: AssetImage("assets/ben.jpg"), radius: 100,)),
+            Divider(height: 90,color: Colors.grey[800],),
+            // SizedBox(height: 40,),
+            Text("NAME",style: TextStyle(color: Colors.grey,letterSpacing: 2),),
+            SizedBox(height: 20,),
+            Text("Serhat cetin",style: TextStyle(color: Colors.amberAccent[200],letterSpacing: 2,fontSize: 28,fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+            Text("POSITION",style: TextStyle(color: Colors.grey,letterSpacing: 2),),
+            SizedBox(height: 20,),
+            Text("TEACHER",style: TextStyle(color: Colors.amberAccent[200],letterSpacing: 2,fontSize: 28,fontWeight: FontWeight.bold),),
+            SizedBox(height: 40,)
+            ,Row(
+              children: [
+                Icon(
+                  Icons.email,color: Colors.grey[400],
+                ),
+                Text("serhat06.sc@gmail.com",style: TextStyle(color: Colors.grey[400],fontSize: 18,),)
+              ],
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
